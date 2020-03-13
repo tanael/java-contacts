abefc519eb6b3fe8d6dce57e58892604bc870aba
Nathan Lövsund <nathan@lovsund.eu>
2020-03-13 22:48:49 +0100

    Update main code
    Minor update, REBASE

--- Contacts.java
+++ Contacts.java
@@ -1,5 +1,31 @@
+// Contact list entity
 class Contacts {
-    public static void main(String[] arguments) {
-        System.out.println("Contacts");
+    Contact[] contacts;
+
+    public static void addContact() {
+        System.out.println("Adding a contact");
     }
+
+    public static void removeContact() {
+        System.out.println("Removing a contact");
+    }
+
+    // Update?
+
+    public static void main(String[] arguments) {
+
+        if (arguments.length <= 0) {
+            System.out.println("No contacts...");
+            return;
+        }
+
+        System.out.println("Contacts!");
+        for (int idx = 0; idx < arguments.length; idx++) {
+            System.out.println("Contact n°" + idx + ": " + arguments[idx]);
+        }
+    }
+}
+
+// Contact entity
+class Contact {
 }
